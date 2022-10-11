0c1e79da5b92588f37e43803d699a43589d87a81
fakebmarchete <leonardo.ngra@gmail.com>
2022-10-11 07:58:56 -0300

    header finalized

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,26 +1,28 @@
 import styles from './styles.module.scss'
 import Image from 'next/image'
 import logo from '../../../public/images/logo.svg'
-import Link from 'next/link'
+import { ActiveLink } from '../ActiveLink'
 
 export const Header = () => {
     return (
         <header className={styles.headerContainer}>
             <div className={styles.headerContent}>
-                <a>
+                <ActiveLink href='/' activeClassName={styles.active}>
                     <Image src={logo} alt='newspaper logo' />
-                </a>
+                </ActiveLink>
 
                 <nav>
-                    <Link href='/'>
+                    <ActiveLink href='/' activeClassName={styles.active}>
                         <a>Home</a>
-                    </Link>
-                    <Link href='/posts'>
+                    </ActiveLink>
+
+                    <ActiveLink href='/posts' activeClassName={styles.active}>
                         <a>Conteúdos</a>
-                    </Link>
-                    <Link href='/sobre'>
+                    </ActiveLink>
+
+                    <ActiveLink href='/sobre' activeClassName={styles.active}>
                         <a>Quem somos?</a>
-                    </Link>
+                    </ActiveLink>
                 </nav>
 
                 <a className={styles.readyButton} href="https://sujeitoprogramador.com">COMEÇAR</a>
