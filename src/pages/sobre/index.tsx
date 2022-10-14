3e7f6517cea1c7a23fcdf520a34a70a4523f6dbb
fakebmarchete <leonardo.ngra@gmail.com>
2022-10-14 10:34:12 -0300

    pagination about

---
+++ src/pages/sobre/index.tsx
@@ -0,0 +1,92 @@
+import { GetStaticProps } from "next"
+
+import Head from 'next/head'
+import styles from './styles.module.scss'
+
+import { getPrismicClient } from '../../services/prismic'
+import { RichText } from 'prismic-dom'
+import Prismic from '@prismicio/client'
+
+import { FaYoutube, FaInstagram, FaLinkedin, FaFacebook } from 'react-icons/fa'
+
+type Content = {
+    title: string;
+    description: string;
+    banner: string;
+    facebook: string;
+    instagram: string;
+    youtube: string;
+    linkedin: string;
+}
+interface ContentProps {
+    content: Content
+}
+
+export default function Sobre({ content }: ContentProps) {
+    return (
+        <>
+           <Head>
+            <title>Quem somos? | Sujeito Programador</title>
+           </Head>
+           <main className={styles.container}>
+            <div className={styles.containerHeader}>
+            <section className={styles.ctaText}>
+                <h1>{content.title}</h1>
+                <p>{content.description}</p>
+
+                <a href={content.youtube}>
+                    <FaYoutube size={40} />
+                </a>
+                <a href={content.instagram}>
+                    <FaInstagram size={40} />
+                </a>
+                <a href={content.facebook}>
+                    <FaFacebook size={40} />
+                </a>
+                <a href={content.linkedin}>
+                    <FaLinkedin size={40} />
+                </a>
+            </section>
+
+            <img src={content.banner} alt="Sujeito Programador" />
+            </div>
+
+           </main>
+        </>
+    )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+
+    const prismic = getPrismicClient()
+
+    const response = await prismic.query([
+        Prismic.Predicates.at('document.type', 'about')
+    ])
+
+    const {
+        title,
+        description,
+        banner,
+        facebook,
+        instagram,
+        youtube,
+        linkedin
+    } = response.results[0].data;
+
+    const content = {
+        title: RichText.asText(title),
+        description: RichText.asText(description),
+        banner: banner.url,
+        facebook: facebook.url,
+        instagram: instagram.url,
+        youtube: youtube.url,
+        linkedin: linkedin.url,
+    }
+
+    return {
+        props: {
+            content
+        }
+    }
+}
