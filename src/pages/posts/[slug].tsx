96433d2f0f71c5beaf93a521c8fa1b9198a1904b
fakebmarchete <leonardo.ngra@gmail.com>
2022-10-14 09:37:18 -0300

    details posts

---
+++ src/pages/posts/[slug].tsx
@@ -0,0 +1,86 @@
+import { GetServerSideProps } from 'next'
+import styles from './post.module.scss'
+
+import { getPrismicClient } from '../../services/prismic'
+import { RichText } from 'prismic-dom'
+
+import Head from 'next/head'
+import Image from 'next/image'
+
+interface PostProps {
+    post: {
+        slug: string;
+        title: string;
+        description: string;
+        cover: string;
+        updatedAt: string;
+    }
+}
+
+export default function Post({ post }: PostProps) {
+
+
+    return (
+        <>
+            <Head>
+                <title>{post.title}</title>
+            </Head>
+            <main className={styles.container}>
+                <article className={styles.post}>
+                    <Image
+                        src={post.cover}
+                        alt={post.title}
+                        width={720}
+                        height={410}
+                        quality={100}
+                        placeholder="blur"
+                        blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mN0vQgAAWEBGHsgcxcAAAAASUVORK5CYII="
+                    />
+
+                    <h1>{post.title}</h1>
+                    <time>{post.updatedAt}</time>
+                    <div className={styles.postContent} dangerouslySetInnerHTML={{__html: post.description}}>
+
+                    </div>
+                </article>
+            </main>
+        </>
+    )
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ req, params }) => {
+
+    const { slug } = params;
+    const prismic = getPrismicClient(req)
+
+    const response = await prismic.getByUID('post', String(slug), {})
+
+    //console.log(response.data)
+
+    if (!response) {
+        return {
+            redirect: {
+                destination: '/posts',
+                permanent: false
+            }
+        }
+    }
+
+    const post = {
+        slug: slug,
+        title: RichText.asText(response.data.title),
+        description: RichText.asHtml(response.data.description),
+        cover: response.data.cover.url,
+        updatedAt: new Date(response.last_publication_date).toLocaleDateString('pt-BR', {
+            day: '2-digit',
+            month: 'long',
+            year: 'numeric'
+        })
+    }
+
+    return {
+        props: {
+            post
+        }
+    }
+}
