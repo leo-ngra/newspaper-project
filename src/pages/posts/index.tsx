ca636d1908257f8046ef386aed60a8e42f22bf43
fakebmarchete <leonardo.ngra@gmail.com>
2022-10-14 08:35:29 -0300

    pagination

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,115 +1,172 @@
-import { GetStaticProps } from 'next'
-import { useState } from 'react'
-import Head from 'next/head'
-import Link from 'next/link'
-import styles from './styles.module.scss'
-import Image from 'next/image'
-import { FiChevronLeft, FiChevronsLeft, FiChevronRight, FiChevronsRight } from 'react-icons/fi'
-import { getPrismicClient } from '../../services/prismic'
-import Prismic from '@prismicio/client'
-import { RichText } from 'prismic-dom'
+import { GetStaticProps } from 'next';
+import { useState } from 'react';
 
+import Head from 'next/head';
+
+import styles from './styles.module.scss';
+import Link from 'next/link';
+
+import Image from 'next/image';
+
+import { getPrismicClient } from '../../services/prismic';
+import Prismic from '@prismicio/client';
+import { RichText} from 'prismic-dom'; 
+
+// https://png-pixel.com/
+
+import { FiChevronLeft, FiChevronsLeft, FiChevronRight, FiChevronsRight} from 'react-icons/fi';
 
 type Post = {
-    slug: string;
-    title: string;
-    cover: string;
-    description: string;
-    updatedAt: string;
-}
-interface PostsProps {
-    posts: Post[];
+  slug: string;
+  title: string;
+  cover: string;
+  description: string;
+  updatedAt: string
 }
 
-const Posts = ({ posts: postsBlog }: PostsProps) => {
-
-    const [posts, setPosts] = useState(postsBlog || [])
-
-    return (
-        <>
-            <Head>
-                <title>Blog | Posts</title>
-            </Head>
-            <main className={styles.container}>
-                <div className={styles.posts}>
-
-                    {postsBlog.map(post => (
-                        <Link key={post.slug} href={`/posts/${post.slug}`}>
-                            <a key={post.slug}>
-                                <Image
-                                    src={post.cover}
-                                    alt={post.title}
-                                    width={720}
-                                    height={410}
-                                    quality={100}
-                                    blurDataURL='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mMUFhVfDwABcgDwbsbrTQAAAABJRU5ErkJggg=='
-                                    placeholder='blur' />
-                                <strong>{post.title}</strong>
-                                <time>{post.updatedAt}</time>
-                                <p>{post.description}</p>
-                            </a>
-                        </Link>
-                    ))}
-
-                    <div className={styles.buttonNavigate}>
-                        <div>
-                            <button>
-                                <FiChevronsLeft size={25} color='#FFF' />
-                            </button>
-                            <button>
-                                <FiChevronLeft size={25} color='#FFF' />
-                            </button>
-                        </div>
-
-                        <div>
-                            <button>
-                                <FiChevronsRight size={25} color='#FFF' />
-                            </button>
-                            <button>
-                                <FiChevronRight size={25} color='#FFF' />
-                            </button>
-                        </div>
-                    </div>
-                </div>
-            </main>
-        </>
-    )
+interface PostsProps{
+  posts: Post[];
+  page: string;
+  totalPage: string;
 }
 
-export default Posts
+export default function Posts({ posts: postsBlog, page, totalPage }: PostsProps){
 
-export const getStaticProps: GetStaticProps = async () => {
+  const [currentPage, setCurrentPage] = useState(Number(page)); 
+  const [posts, setPosts] = useState(postsBlog || []);
 
-    const prismic = getPrismicClient()
+  //Buscar novos posts
+  async function reqPost(pageNumber: number){
+    const prismic = getPrismicClient();
 
     const response = await prismic.query([
-        Prismic.Predicates.at('document.type', 'post')
+      Prismic.Predicates.at('document.type', 'post')
     ], {
-        orderings: '[document.last_publication_date desc]',
-        fetch: ['post.title', 'post.description', 'post.cover'],
-        pageSize: 3
+      orderings: '[document.last_publication_date desc]', //Ordenar pelo mais recente
+      fetch: ['post.title', 'post.description', 'post.cover'],
+      pageSize: 3,
+      page: String(pageNumber)  
     })
 
-    //console.log(JSON.stringify(response, null,2))
+    return response;
+  }
 
-    const posts = response.results.map(post => {
-        return {
-            slug: post.uid,
-            title: RichText.asText(post.data.title),
-            description: post.data.description.find(content => content.type === 'paragraph')?.text ?? '',
-            cover: post.data.cover.url,
-            updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
-                day: '2-digit',
-                month: 'long',
-                year: 'numeric'
-            })
-        }
-    })
+  async function navigatePage(pageNumber: number){
+    const response = await reqPost(pageNumber);
 
-    return {
-        props: {
-            posts
-        },
-        revalidate: 60 * 30 // 30 minutes
+    if(response.results.length === 0){
+    return;      
     }
+
+    const getPosts = response.results.map( post => {
+      return {
+        slug: post.uid,
+        title: RichText.asText(post.data.title),
+        description: post.data.description.find(content => content.type === 'paragraph')?.text ?? '',
+        cover: post.data.cover.url,
+        updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
+          day: '2-digit',
+          month: 'long',
+          year: 'numeric'
+        })
+      }
+    })
+
+    setCurrentPage(pageNumber)
+    setPosts(getPosts);
+
+  }
+
+  return(
+    <>
+     <Head>
+       <title>Blog | Sujeito Programador</title>
+     </Head>
+     <main className={styles.container}>
+       <div className={styles.posts}>
+        {posts.map( post => (
+          <Link key={post.slug} href={`/posts/${post.slug}`}>
+            <a key={post.slug}>
+              <Image 
+                src={post.cover} 
+                alt={post.title}
+                width={720}
+                height={410}
+                quality={100}
+                placeholder="blur"
+                blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mN0vQgAAWEBGHsgcxcAAAAASUVORK5CYII="
+              />
+              <strong>{post.title}</strong>
+              <time>{post.updatedAt}</time>
+              <p>{post.description}</p>
+            </a>
+          </Link>
+        ))}
+
+         <div className={styles.buttonNavigate}>
+            { Number(currentPage) >= 2 && (
+              <div>
+                <button onClick={ () => navigatePage(1) }>
+                  <FiChevronsLeft size={25} color="#FFF" />
+                </button>
+                <button onClick={ () => navigatePage(Number(currentPage - 1)) }>
+                  <FiChevronLeft size={25} color="#FFF" />
+                </button>
+              </div>
+            )}
+
+            { Number(currentPage) < Number(totalPage) && (
+              <div>
+                <button onClick={ () => navigatePage(Number(currentPage + 1)) }>
+                  <FiChevronRight size={25} color="#FFF" />
+                </button>
+                <button onClick={ () => navigatePage(Number(totalPage)) }>
+                  <FiChevronsRight size={25} color="#FFF" />
+                </button>
+              </div>
+            )}
+            
+         </div>
+       </div>
+     </main>
+    </>
+  )
+}
+
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+
+  const response = await prismic.query([
+    Prismic.Predicates.at('document.type', 'post')
+  ], {
+    orderings: '[document.last_publication_date desc]', //Ordenar pelo mais recente
+    fetch: ['post.title', 'post.description', 'post.cover'],
+    pageSize: 3
+  })
+
+  // console.log(JSON.stringify(response, null, 2))
+
+  const posts = response.results.map( post => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      description: post.data.description.find(content => content.type === 'paragraph')?.text ?? '',
+      cover: post.data.cover.url,
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric'
+      })
+    }
+  })
+
+  return{
+    props:{
+      posts,
+      page: response.page,
+      totalPage: response.total_pages
+    },
+    revalidate: 60 * 30 // Atualiza a cada 30 minutos.
+  }
 }
